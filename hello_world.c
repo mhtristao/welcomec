def1db19a07b617532ed6eae688fe2ee78ffff88
mhtristao <mariohortatristao@gmail.com>
2026-07-10 16:39:44 -0300

    primeira atualizacao do arquivo feito no desktop

--- hello_world.c
+++ hello_world.c
@@ -2,6 +2,7 @@
 
 int main(){
 	std::cout << "ola, mundo! voce esta integrando seu programinha em c com git" << std::endl;
+	std::cout << "vou comecar aqui no desktop hoje!"<<std::endl;
 	return 0;
 
 }
